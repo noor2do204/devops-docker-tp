2fd6c028fafda43be653aec7308e4ab64a355a9d
HHibnmudir <achraf.dabach59@gmail.com>
2024-07-04 17:24:56 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
-# This script is used to create a timestamp file and write the current date and time into it.
+# Ce script est utilisé pour créer un fichier de timestamp et y écrire la date et l'heure actuelles.
 
 set -eux
 # Setting shell options for better error handling and debugging.
 # 'e' option will cause the shell to exit if any invoked command fails.
 # 'u' option treats unset variables and parameters as an error.
 # 'x' option prints each command that gets executed to the terminal, useful for debugging.
-
-touch /home/www/started.time
-# This command creates a file named 'started.time' in the '/home/www' directory.
-# If the file already exists, the command does not change the file but updates its access and modification timestamps.
+touch /tmp/started.time
+# Cette commande crée un fichier nommé 'started.time' dans le répertoire '/home/www'.
+# Si le fichier existe déjà, la commande ne le modifie pas mais met à jour ses horodatages d'accès et de modification.
 
 if [ $? -ne 0 ]; then
     exit
 fi
-# This conditional statement checks the exit status of the last command (touch command in this case).
-# If the exit status is not zero, which indicates an error, the script will exit immediately.
 
-date > /home/www/started.time
-# This command writes the current date and time to the 'started.time' file.
-# The '>' operator is used to redirect the output of the 'date' command to the file.
-
+date > /tmp/started.time
 exec "$@"
-# This command replaces the current shell process with the command given as argument.
-# It's used to pass control from the script to another program or command.
-# The "$@" is a special variable that holds all the arguments passed to the script.
+#   j'ai modifiÃ© le chemin (au lieu de /home/www    j'ai mit /tmp car pas besoin de droit)
